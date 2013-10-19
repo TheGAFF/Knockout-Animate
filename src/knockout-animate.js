f45e0dfad8d88151447990afcf05b97c61ebec22
John Gafford <john@johngafford.com>
2013-10-18 19:19:53 -0500

    Added easing

--- src/knockout-animate.js
+++ src/knockout-animate.js
@@ -10,28 +10,25 @@ var koAnimate =
             scale: 1.2,
             duration: 250,
             scaleOut: 1.0,
-            durationOut: 100
+            durationOut: 100,
+            easing: 'ease-in',
+            easingOut: 'ease-out'
         },
         hoverRotate:
         {
             degrees: 30,
             duration: 250,
             degreesOut: 0,
-            durationOut: 100
-        },
-        hoverColor: 
-        {
-            color: 'black',
-            colorOut: 'white',
-            background: 'white',
-            backgroundOut: 'black',
-            duration: 500,
-            durationOut: 500,
+            durationOut: 100,
+            easing: 'ease-in',
+            easingOut: 'ease-out'
         },
         fadeVisible:
         {
             duration: 500,
-            durationOut: 500
+            durationOut: 500,
+            easing: 'ease-in',
+            easingOut: 'ease-out'
         },
         scaleVisible:
         {
@@ -39,14 +36,18 @@ var koAnimate =
             scaleHide: 0,
             scaleHideOut: 0,
             duration: 500,
-            durationOut: 500
+            durationOut: 500,
+            easing: 'ease-in',
+            easingOut: 'ease-out'
         },
         slideVisible:
         {
             duration: 500,
             durationOut: 500,
             direction: 'left',
-            directionOut: 'left'
+            directionOut: 'left',
+            easing: 'ease-in',
+            easingOut: 'ease-out'
         },
         cssAnimate:
         {
@@ -181,39 +182,39 @@ koAnimate.animations =
         $(element).css('transition', 'none');
     },
 
-    scale: function (element, scale, duration)
+    scale: function (element, scale, duration, easing)
     {
         var seconds = duration / 1000;
         $.each(koAnimate.helpers.cssVendors, function (index, item)
         {
-            $(element).css(item + 'transform', 'scale(' + scale + ')').css(item + 'transition', seconds + 's ease');
+            $(element).css(item + 'transform', 'scale(' + scale + ')').css(item + 'transition', seconds + 's ' + easing);
         });
     },
 
-    rotate: function (element, degrees, duration)
+    rotate: function (element, degrees, duration, easing)
     {
         var seconds = duration / 1000;
         $.each(koAnimate.helpers.cssVendors, function (index, item)
         {
-            $(element).css(item + 'transform', 'rotate(' + degrees + 'deg)').css(item + 'transition', seconds + 's ease');
+            $(element).css(item + 'transform', 'rotate(' + degrees + 'deg)').css(item + 'transition', seconds + 's ' + easing);
         });
     },
 
-    opacity: function (element, opacity, duration)
+    opacity: function (element, opacity, duration, easing)
     {
         var seconds = duration / 1000;
         $.each(koAnimate.helpers.cssVendors, function (index, item)
         {
-            $(element).css("opacity", opacity).css(item + 'transition', 'opacity ' + seconds + 's ease');
+            $(element).css("opacity", opacity).css(item + 'transition', 'opacity ' + seconds + 's ' + easing);
         });
     },
 
-    slide: function (element, pixelsX, pixelsY, duration)
+    slide: function (element, pixelsX, pixelsY, duration, easing)
     {
         var seconds = duration / 1000;
         $.each(koAnimate.helpers.cssVendors, function (index, item)
         {
-            $(element).css(item + 'transform', 'translate(' + pixelsX + ',' + pixelsY + ')').css(item + 'transition', seconds + 's ease');
+            $(element).css(item + 'transform', 'translate(' + pixelsX + ',' + pixelsY + ')').css(item + 'transition', seconds + 's ' + easing);
         });
     }
 };
@@ -230,16 +231,18 @@ ko.bindingHandlers.hoverScale =
         var duration = koAnimate.helpers.getValue(json, "duration", koAnimate.defaults.hoverScale.duration, true);
         var scaleOut = koAnimate.helpers.getValue(json, "scaleOut", koAnimate.defaults.hoverScale.scaleOut, true);
         var durationOut = koAnimate.helpers.getValue(json, "durationOut", koAnimate.defaults.hoverScale.durationOut, true);
+        var easing = koAnimate.helpers.getValue(json, "easing", koAnimate.defaults.hoverScale.easing, true);
+        var easingOut = koAnimate.helpers.getValue(json, "easingOut", koAnimate.defaults.hoverScale.easingOut, true);
 
         ko.utils.registerEventHandler(element, "mouseenter", function ()
         {
 
-            koAnimate.animations.scale(element, scale, duration);
+            koAnimate.animations.scale(element, scale, duration, easing);
         });
 
         ko.utils.registerEventHandler(element, "mouseleave", function ()
         {
-            koAnimate.animations.scale(element, scaleOut, durationOut);
+            koAnimate.animations.scale(element, scaleOut, durationOut, easingOut);
         });
     }
 };
@@ -252,19 +255,20 @@ ko.bindingHandlers.hoverRotate =
         var duration = koAnimate.helpers.getValue(json, "duration", koAnimate.defaults.hoverRotate.duration, true);
         var degreesOut = koAnimate.helpers.getValue(json, "degreesOut", koAnimate.defaults.hoverRotate.degreesOut, true);
         var durationOut = koAnimate.helpers.getValue(json, "durationOut", koAnimate.defaults.hoverRotate.durationOut, true);
+        var easing = koAnimate.helpers.getValue(json, "easing", koAnimate.defaults.hoverRotate.easing, true);
+        var easingOut = koAnimate.helpers.getValue(json, "easingOut", koAnimate.defaults.hoverRotate.easingOut, true);
 
         ko.utils.registerEventHandler(element, "mouseenter", function ()
         {
-            koAnimate.animations.rotate(element, degrees, duration);
+            koAnimate.animations.rotate(element, degrees, duration, easing);
         });
 
         ko.utils.registerEventHandler(element, "mouseleave", function ()
         {
-            koAnimate.animations.rotate(element, degreesOut, durationOut);
+            koAnimate.animations.rotate(element, degreesOut, durationOut, easingOut);
         });
     }
 };
-
 
 
 //#endregion "Hovering"
@@ -276,7 +280,7 @@ ko.bindingHandlers.fadeVisible =
     init: function (element, json)
     {
         var observable = koAnimate.helpers.getValue(json, "observable", null, null, true);
-
+        
         if (ko.utils.unwrapObservable(observable))
         {
             $(element).show();
@@ -286,7 +290,7 @@ ko.bindingHandlers.fadeVisible =
             $(element).hide();
             
             //Set initial opacity
-            koAnimate.animations.opacity(element, 0, 0);
+            koAnimate.animations.opacity(element, 0, 0, 'ease');
         }
     },
     update: function (element, json)
@@ -294,6 +298,8 @@ ko.bindingHandlers.fadeVisible =
         var observable = koAnimate.helpers.getValue(json, "observable", null, null, true);
         var duration = koAnimate.helpers.getValue(json, "duration", koAnimate.defaults.fadeVisible.duration, true);
         var durationOut = koAnimate.helpers.getValue(json, "durationOut", koAnimate.defaults.fadeVisible.durationOut, true);
+        var easing = koAnimate.helpers.getValue(json, "easing", koAnimate.defaults.fadeVisible.easing, true);
+        var easingOut = koAnimate.helpers.getValue(json, "easingOut", koAnimate.defaults.fadeVisible.easingOut, true);
         
         $(element).off(koAnimate.animations.transitionEnd);
         
@@ -303,7 +309,7 @@ ko.bindingHandlers.fadeVisible =
             
             element.koAnimateFadeVisible = setTimeout(function()
             {
-                koAnimate.animations.opacity(element, 1, duration);
+                koAnimate.animations.opacity(element, 1, duration, easing);
                 
                 $(element).on(koAnimate.animations.transitionEnd, function ()
                 {
@@ -317,7 +323,7 @@ ko.bindingHandlers.fadeVisible =
         {
             $(element).show();
             
-            koAnimate.animations.opacity(element, 0, durationOut);
+            koAnimate.animations.opacity(element, 0, durationOut, easingOut);
             
             $(element).on(koAnimate.animations.transitionEnd, function ()
             {
@@ -350,6 +356,8 @@ ko.bindingHandlers.scaleVisible =
         var scale = koAnimate.helpers.getValue(json, "scale", koAnimate.defaults.scaleVisible.scale, true);
         var scaleHide = koAnimate.helpers.getValue(json, "scaleHide", koAnimate.defaults.scaleVisible.scaleHide, true);
         var scaleHideOut = koAnimate.helpers.getValue(json, "scaleHideOut", koAnimate.defaults.scaleVisible.scaleHideOut, true);
+        var easing = koAnimate.helpers.getValue(json, "easing", koAnimate.defaults.scaleVisible.easing, true);
+        var easingOut = koAnimate.helpers.getValue(json, "easingOut", koAnimate.defaults.scaleVisible.easingOut, true);
 
         $(element).off(koAnimate.animations.transitionEnd);
 
@@ -362,7 +370,7 @@ ko.bindingHandlers.scaleVisible =
             setTimeout(function()
             {
                 
-                koAnimate.animations.scale(element, scale, duration);
+                koAnimate.animations.scale(element, scale, duration, easing);
                 
                 $(element).on(koAnimate.animations.transitionEnd, function ()
                 {
@@ -374,7 +382,7 @@ ko.bindingHandlers.scaleVisible =
         }
         else
         {
-            koAnimate.animations.scale(element, scaleHideOut, durationOut);
+            koAnimate.animations.scale(element, scaleHideOut, durationOut, easingOut);
             
             $(element).on(koAnimate.animations.transitionEnd, function ()
             {
@@ -399,7 +407,7 @@ ko.bindingHandlers.slideVisible =
         else
         {
             $(element).hide();
-            koAnimate.animations.slide(element, koAnimate.helpers.getDirectionX(directionOut), 0);
+            koAnimate.animations.slide(element, koAnimate.helpers.getDirectionX(directionOut), 0, 'ease');
         }
     },
     update: function (element, json)
@@ -408,6 +416,8 @@ ko.bindingHandlers.slideVisible =
         var duration = koAnimate.helpers.getValue(json, "duration", koAnimate.defaults.slideVisible.duration, true);
         var durationOut = koAnimate.helpers.getValue(json, "durationOut", koAnimate.defaults.slideVisible.durationOut, true);
         var directionOut = koAnimate.helpers.getValue(json, "directionOut", koAnimate.defaults.slideVisible.directionOut, true);
+        var easing = koAnimate.helpers.getValue(json, "easing", koAnimate.defaults.slideVisible.easing, true);
+        var easingOut = koAnimate.helpers.getValue(json, "easingOut", koAnimate.defaults.slideVisible.easingOut, true);
 
         $(element).off(koAnimate.animations.transitionEnd);
         
@@ -419,14 +429,14 @@ ko.bindingHandlers.slideVisible =
                 
                 setTimeout(function ()
                 {
-                    koAnimate.animations.slide(element, '0px', '0px', duration);
+                    koAnimate.animations.slide(element, '0px', '0px', duration, easing);
                 }, 50);
             }
             else
             {
                 setTimeout(function ()
                 {
-                    koAnimate.animations.slide(element, koAnimate.helpers.getDirectionX(directionOut), koAnimate.helpers.getDirectionY(directionOut), durationOut);
+                    koAnimate.animations.slide(element, koAnimate.helpers.getDirectionX(directionOut), koAnimate.helpers.getDirectionY(directionOut), durationOut, easingOut);
                     
                     $(element).on(koAnimate.animations.transitionEnd, function ()
                     {
